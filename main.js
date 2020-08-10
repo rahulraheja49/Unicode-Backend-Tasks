754d089f9ac840864862c0ac33adf58b930b7a99
rahu49 <rraheja49@gmail.com>
2020-08-10 16:41:36 +0530

    First commit

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-
-
-var first_name = new Array();
-var last_name = new Array();
-var math_marks = new Array();
-var english_marks = new Array();
-
-
 class Student {
     constructor(first_name, last_name, math_marks, english_marks){
         this.first_name = first_name;
@@ -16,25 +8,44 @@ class Student {
     }
 }
 
-var input = ["Rashmil Panchani 99 97", "Parag Vaid 95 93", "Siddarth Sanghvi 98 100"];
 var student = [];
 var students = [];
 var name;
 
-function sortStudents(input) {
-    for(i=0; i<input.length; i++) {
-        students[i] = input[i].split(" ");
-        name = students[i];
-        student[i] = new Student(name[0], name[1], name[2], name[3]);            
-    }
-    student.sort(function(a,b){
-        return b.average-a.average;
-    });
-    console.log('[');
-    for(i=0; i<(input.length-1); i++) {
-        console.log(`{\n    Name: ${student[i].first_name} ${student[i].last_name}\n        Score: {\n          Maths: ${student[i].math_marks},\n          English: ${student[i].english_marks}\n        }\n},`);
-    }   
-    console.log(']');
+
+const fs = require('fs');
+
+function loadJSON(filename = '') {
+    return JSON.parse(
+        fs.existsSync(filename)
+        ? fs.readFileSync(filename).toString()
+        : '"'
+    )
 }
 
-sortStudents(input);
+function addJSON(filename = '', json = '"'){
+    return fs.writeFileSync(
+        filename,
+        JSON.stringify(json, null, 2)
+    )
+}
+
+const data = loadJSON('data.json');
+
+var input = ["Rashmil Panchani 99 97", "Parag Vaid 95 93", "Siddharth Sanghavi 98 100"];
+for(i=0; i<input.length; i++) {
+    students[i] = input[i].split(" ");
+    name = students[i];
+    student[i] = new Student(name[0], name[1], name[2], name[3]);            
+}
+student.sort(function(a,b){
+    return b.average-a.average;
+});
+
+[student].forEach(child =>
+    data.students.push(child)
+)
+
+addJSON('data.json', data)
+
+console.log(data.students);
